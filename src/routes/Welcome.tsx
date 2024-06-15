c811a28305165f0a310433e0b885fcc8e30a7a90
davidguri <david.guri@icloud.com>
2024-06-15 17:02:51 +0200

    ...

--- src/routes/Welcome.tsx
+++ src/routes/Welcome.tsx
@@ -4,7 +4,21 @@ export default function WelcomePage() {
   return (
     <>
       <main className={styles.main}>
-        <text style={{ color: "white" }}>Welcome</text>
+        <text className={styles.title}>Welcome To Legera</text>
+        <div className={styles.buttonContainer}>
+          <div className={styles.inlineContainer}>
+            <div className={styles.button}>
+              <text className={styles.buttonText}>Log In</text>
+            </div>
+            <div className={styles.button}>
+              <text className={styles.buttonText}>Sign Up</text>
+            </div>
+          </div>
+          <text className={styles.orText}>or</text>
+          <div className={styles.button}>
+            <text className={styles.buttonText}>Google</text>
+          </div>
+        </div>
       </main>
     </>
   );
