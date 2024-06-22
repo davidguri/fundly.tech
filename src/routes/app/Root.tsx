3eb6434e4c251a62a6ec4e70e483ff6172af12d8
davidguri <david.guri@icloud.com>
2024-06-22 18:00:39 +0300

    working on home page design

--- src/routes/app/Root.tsx
+++ src/routes/app/Root.tsx
@@ -1,12 +1,43 @@
 import styles from "./styles/Root.module.scss";
 import Layout from "../../components/layout/Layout";
 
+import { IoArrowForward, IoAdd, IoCalendar, IoRepeat, IoWallet } from "react-icons/io5";
+
+import Svg from "../../assets/react.svg";
+
 export default function Root() {
   return (
     <>
       <Layout>
         <main className={styles.main}>
-          Root Page
+          <section className={styles.topSection}>
+            <div className={styles.topContainer}>
+              <div className={styles.titleContainer}>
+                <text className={styles.subtitle}>Current Month</text>
+                <text className={styles.title}>$200,5</text>
+              </div>
+              <div className={styles.profileContainer}>
+                <img src={Svg} className={styles.profileImage} alt="profile_image" />
+              </div>
+            </div>
+          </section>
+          <section className={styles.bottomSection}>
+            <div className={styles.buttonContainer}>
+              <IoWallet className={styles.buttonIcon} />
+            </div>
+            <div className={styles.buttonContainer}>
+              <IoCalendar className={styles.buttonIcon} />
+            </div>
+            <div className={styles.buttonContainer}>
+              <IoAdd size={45} />
+            </div>
+            <div className={styles.buttonContainer}>
+              <IoRepeat className={styles.buttonIcon} />
+            </div>
+            <div className={styles.buttonContainer}>
+              <IoArrowForward className={styles.buttonIcon} />
+            </div>
+          </section>
         </main>
       </Layout>
     </>
